0f396670fa03fdee7cd0f31a0aae230f1f32ce8d
Carlos Duque <carlosduque.hn@gmail.com>
2014-11-27 23:29:23 -0300

    added JAVA_HOME for mac os x

--- bin/setup_path_variable.sh
+++ bin/setup_path_variable.sh
@@ -2,6 +2,7 @@
 
 export BASE_PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin"
 if [[ "$OSTYPE" == "darwin"* ]]; then
+    export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
     export CUSTOM_PATH="/Users/carlos/.rvm/bin:/Users/carlos/.rvm/gems/ruby-1.9.3-p484/bin"
     export CUSTOM_PATH=$CUSTOM_PATH:"/usr/local/git/bin:/usr/local/MacGPG2/bin"
     export CUSTOM_PATH=$CUSTOM_PATH:"/Library/Frameworks/JRuby.framework/Versions/Current/bin"
@@ -14,9 +15,9 @@ elif [[ "$OSTYPE" == "cygwin" ]]; then
     export ANT_HOME="$WINAPPS_HOME\\Java\\apache-ant-1.9.4"
     export MAVEN_HOME="$WINAPPS_HOME\\Java\\apache-maven-3.2.3"
     export CUSTOM_PATH="$JAVA_HOME\\bin:$ANT_HOME\\bin:$MAVEN_HOME\\bin"
-    export CUSTOM_PATH=$CUSTOM_PATH:"C:\\WINDOWS:C:\\WINDOWS\\System32"
-    export CUSTOM_PATH=$CUSTOM_PATH:"C:\\WINDOWS\\System32\\WBEM:C:\\WINDOWS\\SYSTEM32\\WINDOWSPOWERSHELL\\V1.0\\"
-    export CUSTOM_PATH=$CUSTOM_PATH:"$WINAPPS_HOME\\HashiCorp\\Vagrant\\bin"
+    export CUSTOM_PATH="$CUSTOM_PATH:C:\\WINDOWS:C:\\WINDOWS\\System32"
+    export CUSTOM_PATH="$CUSTOM_PATH:C:\\WINDOWS\\System32\\WBEM:C:\\WINDOWS\\SYSTEM32\\WINDOWSPOWERSHELL\\V1.0\\"
+    export CUSTOM_PATH="$CUSTOM_PATH:$WINAPPS_HOME\\HashiCorp\\Vagrant\\bin"
 else
     #unknown
     echo
