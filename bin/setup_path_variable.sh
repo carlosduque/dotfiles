c401eb28036d6f3d9734ba923694523c33e3775a
Carlos Duque <carlosduque.hn@gmail.com>
2014-12-31 11:51:26 -0300

    added weblogic env variables

--- bin/setup_path_variable.sh
+++ bin/setup_path_variable.sh
@@ -6,12 +6,14 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
     export OPENEJB_HOME=~/srv/openejb/inst
     export CATALINA_HOME=~/srv/tomcat/inst
+    export MW_HOME=~/srv/weblogic/inst
     export CUSTOM_PATH="$(brew --prefix coreutils)/libexec/gnubin"
     export CUSTOM_PATH=$CUSTOM_PATH:"/Users/carlos/.rvm/bin:/Users/carlos/.rvm/gems/ruby-1.9.3-p484/bin"
     export CUSTOM_PATH=$CUSTOM_PATH:"/usr/local/git/bin:/usr/local/MacGPG2/bin"
     export CUSTOM_PATH=$CUSTOM_PATH:"/Library/Frameworks/JRuby.framework/Versions/Current/bin"
     export CUSTOM_PATH=$CUSTOM_PATH:"$OPENEJB_HOME/bin"
     export CUSTOM_PATH=$CUSTOM_PATH:"$CATALINA_HOME/bin"
+    export CUSTOM_PATH=$CUSTOM_PATH:"$MW_HOME/bin"
     #elif [[ "$OSTYPE" == "freebsd"* ]]; then
     #    export CUSTOM_PATH="~/bin"
 elif [[ "$OSTYPE" == "cygwin" ]]; then
@@ -30,8 +32,9 @@ else
     export MAVEN_HOME=/usr/share/maven
     export OPENEJB_HOME=/srv/openejb/inst
     export CATALINA_HOME=/srv/tomcat/inst
+    export MW_HOME=/srv/weblogic/inst
     export CUSTOM_PATH="$JAVA_HOME/bin:$ANT_HOME/bin:$MAVEN_HOME/bin:$OPENEJB_HOME/bin"
-    export CUSTOM_PATH="$CUSTOM_PATH:$CATALINA_HOME/bin"
+    export CUSTOM_PATH="$CUSTOM_PATH:$CATALINA_HOME/bin:$MW_HOME/bin"
 fi
 
 export PATH=$PATH:$BASE_PATH:$CUSTOM_PATH
