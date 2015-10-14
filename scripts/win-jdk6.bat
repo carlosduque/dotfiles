f590d9da0c05b93a91a004a7a4457fa23b7c4e93
Carlos Duque <carlosduque.hn@gmail.com>
2015-10-14 15:57:37 -0300

    added some win related scripts

---
+++ scripts/win-jdk6.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Setting JAVA_HOME
+set JAVA_HOME=C:\apps\Java\jdk1.6.0_45
+echo setting PATH
+set PATH=%JAVA_HOME%\bin;%PATH%
+echo Display java version
+java -version
+
---
+++ scripts/win-jdk7.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Setting JAVA_HOME
+set JAVA_HOME=C:\apps\Java\jdk1.7.0_71
+echo setting PATH
+set PATH=%JAVA_HOME%\bin;%PATH%
+echo Display java version
+java -version
+
