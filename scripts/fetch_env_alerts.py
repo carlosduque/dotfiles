052f5fe99dfafdfa989136232756114a54740658
Carlos Duque <carlosduque.hn@gmail.com>
2016-06-22 18:09:37 -0300

    environmental alerts script fully working

--- scripts/fetch_env_alerts.py
+++ scripts/fetch_env_alerts.py
@@ -7,31 +7,44 @@ import re
 from bs4 import BeautifulSoup
 import requests
 
+MY_PLATES_LAST_NUM = 5
+
 url = "http://www.uoct.cl/restriccion-vehicular/"
+#url = "http://www.beastie.me/restrict.html"
+
 def extract_greens(text):
-    seal_sections = text.split(",")
-    greens = re.search(r'[0-9].*[0-9]$', seal_sections[1], flags = 0)
-    green_restrictions = [int(num) for num in greens.group.split("-")]
+    'Extract restricted plates with green seals'
+    # from pudb import set_trace; set_trace()
+    seals = text.split(",")
+    greens = re.search(r'[0-9].*[0-9]$', seals[1], flags = 0)
+    restrictions = [int(num) for num in greens.group().split("-")]
+    return restrictions
 
 def extract_date(text):
-    text.split(":")[0]
+    'Extract the date that this restriction applies to'
+    return text.split(":")[0]
 
 def main():
     req = None
     try:
+        print "[==>] 'GET' %s" % (url)
         req = requests.get(url)
         if req.status_code != 200:
-            print "received: %s" % (req.status_code)
+            print "[!!!] Received: %s" % (req.status_code)
             sys.exit(-1)
         else:
-            html = BeautifulSoup(req.txt, "html.parser")
+            html = BeautifulSoup(req.text, "html.parser")
             #html.select(".preemergencia")
             #html.find_all(re.compile("^span"))
-            preemergency = html.find("span", class_="preemergencia").text
-            date = preemergency.split(":")[0]
-            restricted = extract_greens(preemergency.split(":")[1])
-            print "restrictions: %s" % (restricted)
+            alert_text = html.find("span", class_="preemergencia").text
+            date = alert_text.split(":")[0]
+            restricted = extract_greens(alert_text.split(":")[1])
+            print "[***] Restrictions: %s" % (restricted)
     except requests.ConnectionError as rce:
-        print "couldn't connect:%s:%s" % (rce.errno, rce.strerror)
+        print "[!!!] Couldn't connect:%s:%s" % (rce.errno, rce.strerror)
+    except requests.HTTPError as he:
+        print "[!!!] HTTP error:%s:%s" % (he.errno, he.strerror)
 
+    if MY_PLATES_LAST_NUM in restricted:
+        print "You may leave, but your car has to stay home on %s" % (date)
 main()
