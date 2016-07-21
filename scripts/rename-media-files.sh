df379013b944966655b80035ae91842093a99ec1
Carlos Duque <carlosduque.hn@gmail.com>
2016-07-20 21:26:47 -0300

    ssh config settings

--- scripts/rename-media-files.sh
+++ scripts/rename-media-files.sh
@@ -1,12 +1,10 @@
-rename -vf 's/ /_/g' *.mp4
-rename -vf 'y/A-Z/a-z/' *.mp4
-rename -v 's/algorithms/alg/g' *.mp4
-rename -v 's/introduction/intro/g' *.mp4
-rename -v 's/mathematical/math/g' *.mp4
-rename -v 's/observation/obsv/g' *.mp4
-rename -v 's/\(//g' *.mp4
-rename -v 's/\)//g' *.mp4
-rename -v 's/_-_/-/g' *.mp4
-rename -v 's/symbol_table/st/g' *.mp4
-rename -v 's/applications/apps/g' *.mp4
-rename -v 's/_optional//g' *.mp4
+rename -vf 's/ /_/g' *.*
+rename -vf 's/-/_/g' *.*
+rename -v 's/_-_/-/g' *.*
+rename -vf 's/ +/ /g' *.*
+rename -vf 's/_+/_/g' *.*
+rename -vf 's/-+/-/g' *.*
+rename -vf 'y/A-Z/a-z/' *.*
+rename -v 's/\(//g' *.*
+rename -v 's/\)//g' *.*
+rename -v 's/(kidz_bop)-(.+)\.(.+)/$2-$1\.$3/' *.*
