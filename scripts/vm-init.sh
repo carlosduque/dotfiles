048bbba1180ade8e7d2395a598151491ff9c2872
Carlos Duque <carlosduque.hn@gmail.com>
2016-10-14 15:58:43 -0300

    added scala ignores

--- scripts/vm-init.sh
+++ scripts/vm-init.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 echo "mounting shared folders"
-sudo mount -t vboxsf -o rw,gid=1000,uid=1000 dev ~/dev
-sudo mount -t vboxsf -o rw,gid=1000,uid=1000 refs ~/refs
+#sudo mount -t vboxsf -o rw,gid=1000,uid=1000 dev ~/dev
+#sudo mount -t vboxsf -o rw,gid=1000,uid=1000 refs ~/refs
 #sudo mount -t vboxsf -o rw,gid=1000,uid=1000 documents ~/documents
-sudo mount -t vboxsf -o rw,gid=1000,uid=1000 downloads ~/Downloads
+#sudo mount -t vboxsf -o rw,gid=1000,uid=1000 downloads ~/Downloads
+sudo mount -t vboxsf -o rw,gid=1000,uid=1000 share ~/Desktop/share
 echo "shared folders mounted"
 
