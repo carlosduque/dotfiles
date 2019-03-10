00d6cabbd47c47f1a62956343dccbaca20175387
Carlos Duque <carlosduque.hn@gmail.com>
2019-03-09 22:20:58 -0300

    removed emacs.d since it can be downloaded from spacemacs

--- emacs.d/elpa/define-word-20190128.1559/define-word-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "define-word" "20190128.1559" "display the definition of word at point." '((emacs "24.3")) :commit "df1118858fae492cf476a963c1174bfada141c7f" :keywords '("dictionary" "convenience") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/define-word")
