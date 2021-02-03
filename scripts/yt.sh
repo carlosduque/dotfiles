997ad2d6f8eed33bcccdf1fd261a14d77bf915a7
Carlos Duque <carlosduque.hn@gmail.com>
2021-02-03 17:14:33 -0300

    added rename process when downloading from youtube

--- scripts/yt.sh
+++ scripts/yt.sh
@@ -9,5 +9,11 @@ then
     exit
 fi
 
-echo youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --output "%(title)s.%(ext)s" --yes-playlist 'https://www.youtube.com/playlist?list=$PL'
+#echo youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --output "%(title)s.%(ext)s" --yes-playlist 'https://www.youtube.com/playlist?list=$PL'
+youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --output "%(title)s.%(ext)s" --yes-playlist $PL
 
+~/bin/rename.pl -f 'y/A-Z/a-z/' *
+~/bin/rename.pl -v -f 's/ +/ /g' *
+~/bin/rename.pl -v -f 's/ /_/g' *
+~/bin/rename.pl -v -f 's/_\(.*\)//g' *
+~/bin/rename.pl -v -f 's/_\[.*\]//g' *
